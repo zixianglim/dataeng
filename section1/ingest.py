2ec9896d730e60c5173258ae901d6c38c2076e11
zixiang <limzixiang1999@hotmail.com>
2023-08-05 01:24:12 +0800

    added ingest for csv files and upload of dataset1/2/3

--- section1/ingest.py
+++ section1/ingest.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import pandas as pd
 import os
 from datetime import datetime
+import uuid
 
 app = Flask(__name__)
 
@@ -11,25 +12,38 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    name = request.form['name']
-    email = request.form['email']
-    mobile = request.form['mobile']
-    dob = request.form['dob'].replace('-','') 
-        #preprocessing the date as per format before passing into dataset as an example
-        #however, in this case, we will ingest first before cleaning and performing validity checks
-    
-    data = {
-        'applicant_name': [name],
-        'applicant_email': [email],
-        'applicant_mobile': [mobile],
-        'applicant_dob': [dob]
-    }
-    df = pd.DataFrame(data)
+    submission_method = request.form['submission_method']
 
     current_date_hour = datetime.now().strftime('%Y-%m-%d_%H')
-    filename = f'./datasets/member_signup__{current_date_hour}hour.csv'
-    df.to_csv(filename, mode='a', header=not os.path.isfile(filename), index=False)
-        #a new file for each hour irregardless of number of entries
+
+    if submission_method == 'form':
+        name = request.form['name']
+        email = request.form['email']
+        mobile = request.form['mobile']
+        dob = request.form['dob'].replace('-','') 
+            #processing the date as per format if we clean before passing into dataset *as an example*
+            #however, as stated in assignment, we will ingest first before cleaning and performing validity checks
+        
+        data = {
+            'applicant_name': [name],
+            'applicant_email': [email],
+            'applicant_mobile': [mobile],
+            'applicant_dob': [dob]
+        }
+        df = pd.DataFrame(data)
+
+        filename = f'./datasets/member_signup__{current_date_hour}hour.csv'
+        df.to_csv(filename, mode='a', header=not os.path.isfile(filename), index=False)
+            #a new file for each hour irregardless of number of entries
+    
+    elif submission_method == 'csv':
+        file = request.files['csv']
+        if file:
+            filename = file.filename.replace('.csv','')
+            filename = f'./datasets/{filename}_{str(uuid.uuid4())[:5]}_{current_date_hour}hour.csv'
+            #added uuid incase of name duplication
+            
+            file.save(filename)
 
     return 'Your application is being processed. We will get back to you soon :)'
 
